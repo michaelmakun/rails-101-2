1004807d9ca21a4b424ebd7cd0d03fe9d8b556af
makun <michaelmakun@aliyun.com>
2016-12-27 12:43:11 +0800

    modify particpated_groups to participated_groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   has_many :posts
 
   has_many :group_relationships
-  has_many :particpated_groups, :through => :group_relationships, :source => :group
+  has_many :participated_groups, :through => :group_relationships, :source => :group
 
   def is_member_of?(group)
-    particpated_groups.include?(group)
+    participated_groups.include?(group)
   end
 
   def join!(group)
-    particpated_groups << group
+    participated_groups << group
   end
 
   def quit!(group)
-    particpated_groups.delete(group)
+    participated_groups.delete(group)
   end
 
 end
